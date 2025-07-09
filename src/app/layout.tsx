d5ef9bfb8d4e965b03847ccf9121ecf38d0d0242
Somatra <cheysomatra@gmail.com>
2025-07-09 16:56:55 +0700

    deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,25 @@ import Link from 'next/link';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-    title: 'Car Listings App',
-    description: 'A Next.js app for car listings',
+  title: "Car Rental",
+  description: "Car Rental Application built with Next.js and React",
+  keywords: ["car", "discount", "modern", "luxeri", "web development"],
+  authors: [{ name: "Marta Full Stack" }],
+  creator: "Matra",
+  openGraph: {
+    title: "Car Rental",
+    description: "Car Rental Application built with Next.js and React",
+    url: "",
+    siteName: "Car Rental",
+    images: [
+      {
+        url: "https://imgs.search.brave.com/SVYEabTzcQSVZWQl9wiH3bdeXk-0I0RnY3H21gex6sU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jbGlw/YXJ0LWxpYnJhcnku/Y29tL25ld19nYWxs/ZXJ5LzEyMS0xMjE3/NjA4X3Nwb3J0cy1j/YXItY2xpcGFydC1j/YXItY2xpcGFydC13/aXRob3V0LWJhY2tn/cm91bmQucG5n",
+        width: 1200,
+        height: 630,
+        alt: "Car Rental Open Graph Image",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
