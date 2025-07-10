34e053fba93ceed202741630523dce41a8d47458
Somatra <cheysomatra@gmail.com>
2025-07-10 15:55:25 +0700

    deploy

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-export default async function FetchCar(){
+export default async function FetchCar(p0: number){
     const res = await fetch(`https://car-nextjs-api.cheatdev.online/cars`);
 
     const data = await res.json();
